888656cea5659308190da5d77762931ed590a4f4
janosch <janosch@julia.de>
2025-10-28 12:53:29 +0100

    Die Bibliothek 'ONNX' verwendet um ein Naive-Bayes-Model unter C# laufen zu bringen.

--- ONNXBayes/Main.cs
+++ ONNXBayes/Main.cs
@@ -1,25 +1,59 @@
-namespace ONNXBayes
+using Microsoft.ML.OnnxRuntime;
+using Microsoft.ML.OnnxRuntime.Tensors;
 
+namespace ONNXBayes
 {
     // Testing the Naive Bayes model using ONNX in C#
     public class Program
     {
+        private static string _modelPath = "Model/Naive-Bayes-Model.onnx";
+
+
         static void Main()
         {
-            if (!File.Exists("Model/Model.onnx"))
+            if (!File.Exists(_modelPath))
             {
                 Console.WriteLine("Model file not found.");
                 return;
             }
-            
+
             StartingNaiveBayes();
         }
 
         static void StartingNaiveBayes()
         {
+            using var session = new InferenceSession(_modelPath);
 
+            Console.WriteLine("Please enter a message to rate:");
+            string[] message = {Console.ReadLine()};
+            Console.WriteLine($"Your Message: {message[0]}");
 
+            var inputTensor = new DenseTensor<string>(message, new int[] { message.Length, 1 });
+            var inputs = new List<NamedOnnxValue>
+            {
+                NamedOnnxValue.CreateFromTensor("input", inputTensor)
+            };
 
+            using var results = session.Run(inputs);
+
+            var labelTensor = results.First(r => r.Name == "output_label").AsTensor<string>();
+
+            foreach (var prediction in labelTensor)
+            {
+                if (prediction.Equals("spam"))
+                {
+                    Console.WriteLine($"Prediction: {prediction} - Diese Nachricht ist Spam - Bitte diese Nachricht löschen");
+                }
+                else if (prediction.Equals("ham"))
+                {
+                    Console.WriteLine($"Prediction: {prediction} - Diese Nachricht ist kein Spam - Du kannst die Nachricht behalten");
+                }
+                else
+                {
+                    Console.WriteLine("Diese Nachricht kann nicht eingeschätzt werden");
+                }
+
+            }
         }
     }
 }
